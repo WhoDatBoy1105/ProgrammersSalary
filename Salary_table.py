86992c893f085317be4d07ed7a71560254e0107e
WhoDatBoy1105 <111187142+WhoDatBoy1105@users.noreply.github.com>
2025-09-05 21:52:44 +0300

    Update Salary_table.py
    Разбил функцию на составные

--- Salary_table.py
+++ Salary_table.py
@@ -10,37 +10,36 @@ SALARY_TABLE = [
 ]
 
 
-def create_salary_table(vacancies_getter, salary_predictor, title):
+def calculate_vacancies(vacancies_getter, salary_predictor):
     table_data = deepcopy(SALARY_TABLE)
     for programming_language in PROGRAMMING_LANGUAGES:
         vacancies = vacancies_getter(programming_language)
-        predicted_salaries = salary_predictor(vacancies)
+        predicted_salaries = salary_predictor(
+            vacancies)
         if not predicted_salaries:
             continue
-        average_salary = int(sum(predicted_salaries) / len(predicted_salaries))
+        average_salary = int(
+            sum(predicted_salaries) / len(predicted_salaries))
         row = [
             programming_language,
             len(vacancies),
             len(predicted_salaries),
             average_salary
         ]
-        table_data.append(row)
-    table = AsciiTable(table_data, title)
+        table_data.append(row)  # заполеняет таблицу с ваканисями данными из ряда
+    return table_data
+
+
+def create_salary_table(vacancies_getter, salary_predictor, title):
+    table_data = calculate_vacancies(vacancies_getter, salary_predictor)
+    table = AsciiTable(table_data, title)  # создает таблицу с уже заполненными данными
     return table.table
-
-
-def print_hh_salary_table():
-    print(create_salary_table(get_vacancies_hh, predict_rub_salaries_hh, 'HeadHunter Moscow'))
-
-
-def print_sj_salary_table():
-    print(create_salary_table(get_vacancies_sj, predict_rub_salaries_sj, 'SuperJob Moscow'))
 
 
 def main():
     load_dotenv()
-    print_hh_salary_table()
-    print_sj_salary_table()
+    print(create_salary_table(get_vacancies_hh, predict_rub_salaries_hh, 'HeadHunter Moscow'))
+    print(create_salary_table(get_vacancies_sj, predict_rub_salaries_sj, 'SuperJob Moscow'))
 
 
 if __name__ == '__main__':
